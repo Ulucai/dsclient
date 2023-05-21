7b42c1ddcf55ff122b8f8fcbcac953deeaddbbb9
Ulucai <drodrigoandrade@gmail.com>
2023-05-21 13:54:08 -0400

    Client delete, Client hash & equals, Exception Handler

--- backend/src/main/java/com/devsuperior/DSClient/entities/Client.java
+++ backend/src/main/java/com/devsuperior/DSClient/entities/Client.java
@@ -2,6 +2,7 @@ package com.devsuperior.DSClient.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -85,6 +86,23 @@ public class Client implements Serializable{
 
 	public void setChildren(Integer children) {
 		this.children = children;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 	
--- backend/src/main/java/com/devsuperior/DSClient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/DSClient/services/ClientService.java
@@ -3,7 +3,10 @@ package com.devsuperior.DSClient.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -12,48 +15,61 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.DSClient.dto.ClientDTO;
 import com.devsuperior.DSClient.entities.Client;
 import com.devsuperior.DSClient.repositories.ClientRepository;
+import com.devsuperior.DSClient.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
 	@Autowired
 	private ClientRepository repository;
-	
-	@Transactional(readOnly=true)
-	public List<ClientDTO> findAll(){
-		List<Client> list = repository.findAll(); 
-		return list.stream().map(x-> new ClientDTO(x)).toList();
+
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).toList();
 	}
-	
-	@Transactional(readOnly=true)
+
+	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAllPaged(PageRequest pageResquest) {
 		Page<Client> list = repository.findAll(pageResquest);
-		return list.map(x->new ClientDTO(x));
+		return list.map(x -> new ClientDTO(x));
 	}
-	
-	@Transactional(readOnly=true)
+
+	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
-		Optional<Client> result = repository.findById(id); 
-		return new ClientDTO(result.get());
+		Optional<Client> obj = repository.findById(id);
+		Client result = obj.orElseThrow(()->new ResourceNotFoundException("Id" + id + "not found"));
+		return new ClientDTO(result);
 	}
-	
+
 	@Transactional
-	public ClientDTO insert(ClientDTO dto)
-	{
+	public ClientDTO insert(ClientDTO dto) {
 		Client entity = new Client();
 		copyDtoToEntity(entity, dto);
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
 	}
-	
+
 	@Transactional
-	public ClientDTO update(Long id, ClientDTO dto)
-	{
-		Client entity = repository.getOne(id);
-		copyDtoToEntity(entity, dto);		
-		entity = repository.save(entity);
-		return new ClientDTO(entity);
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(entity, dto);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id " + id + " not found");
+		}
 	}
-	
+
+	@Transactional
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id " + id + " not found");
+		}
+	}
+
 	private void copyDtoToEntity(Client entity, ClientDTO dto) {
 		entity.setName(dto.getName());
 		entity.setBirthDate(dto.getBirthDate());
