680687de361152bb38004cb2bbe8de1406fb5424
Ulucai <drodrigoandrade@gmail.com>
2023-05-21 04:03:07 -0400

    FindAllPaged

--- backend/src/main/java/com/devsuperior/DSClient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/DSClient/services/ClientService.java
@@ -3,6 +3,8 @@ package com.devsuperior.DSClient.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.DSClient.dto.ClientDTO;
@@ -17,5 +19,10 @@ public class ClientService {
 	public List<ClientDTO> findAll(){
 		List<Client> list = repository.findAll(); 
 		return list.stream().map(x-> new ClientDTO(x)).toList();
+	}
+
+	public Page<ClientDTO> findAllPaged(PageRequest pageResquest) {
+		Page<Client> list = repository.findAll(pageResquest);
+		return list.map(x->new ClientDTO(x));
 	}
 }
